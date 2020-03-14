dd2929b252e98a5140435272b6023477ae9e6ed4
guoqing.chen <stephen.chan255@gmail.com>
2020-03-14 19:32:00 +1000

    add semantic css

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,7 @@ import React from 'react';
 export default function Footer() {
   return (
     <div className="text-center">
-      <p>&copy; 2020, <a href="https://stephen-chan255.github.io/personal_website/" target="_blank">Stephen Chen</a>. All copyright reserved</p>
+      <p>&copy; 2020, <a href="https://stephen-chan255.github.io/personal_website/" target="_blank" rel="noopener noreferrer">Stephen Chen</a>. All copyright reserved</p>
     </div>
   );
 }
