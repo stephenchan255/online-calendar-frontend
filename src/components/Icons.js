e196b64924be1256662f0579baa187354cf3010b
guoqing.chen <stephen.chan255@gmail.com>
2020-03-15 18:54:07 +1000

    test tool-tip in mobile 3

--- src/components/Icons.js
+++ src/components/Icons.js
@@ -32,7 +32,7 @@ export default function Icons(props) {
         <span data-tip data-for="introduction" style={{ textDecorationLine: 'underline' }}>User Guide</span>
         <ReactTooltip id='introduction' aria-haspopup='true' place="bottom" type="info" effect="solid"
           className="tool-tip-box" disable={isMobile}
-          globalEventOff={isMobile() ? 'click' : undefined}
+          globalEventOff={isMobile ? 'touchstart' : undefined}
         >
           <p>This system is used for employees to schedule future work hours and submit daily work summary.</p>
           <div>
