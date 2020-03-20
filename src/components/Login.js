f5235da0cb8ea793d77094651b7587d30f46fa62
guoqing.chen <stephen.chan255@gmail.com>
2020-03-20 18:10:00 +1000

    style modify & intro useState to Login.js

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,57 +1,48 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import { Redirect } from 'react-router-dom';
 import { postData } from '../services/postData';
 
-export default class Login extends Component {
-  constructor() {
-    super();
-    this.state = {
-      username: '',
-      password: '',
-      redirectToReferrer: false
-    };
-    this.handleInputChange = this.handleInputChange.bind(this);
-    this.login = this.login.bind(this)
+export default function Login() {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [isRedirected, setIsRedirected] = useState(false);
+
+  if (isRedirected) {
+    return (<Redirect to={'/home'} />)
   }
 
-  render() {
-    if (this.state.redirectToReferrer || sessionStorage.getItem('userData')) {
-      return (<Redirect to={'/home'} />)
-    }
-
-    return (
-      <div className="content">
-        <div className="form">
-          <div className="form-group">
-            <input type="text" name="username" placeholder="Username"
-              onChange={this.handleInputChange} className="form-control" />
-          </div>
-          <div className="form-group">
-            <input type="password" name="password" placeholder="Password"
-              onChange={this.handleInputChange} className="form-control" />
-          </div>
-          <input type="submit" value="Login" onClick={this.login} className="btn" />
-          <a href="/signup">Signup</a>
+  return (
+    <div className="content">
+      <div className="form">
+        <div className="form-group">
+          <input type="text" name="username" placeholder="Username"
+            onChange={(e) => handleInputChange(setUsername, e)} className="form-control" />
         </div>
+        <div className="form-group">
+          <input type="password" name="password" placeholder="Password"
+            onChange={(e) => handleInputChange(setPassword, e)} className="form-control" />
+        </div>
+        <input type="submit" value="Login" onClick={() => loginCheck(username, password, setIsRedirected)} className="btn" />
+        <a href="/signup">Signup</a>
       </div>
-    );
-  }
+    </div>
+  );
+}
 
-  login() {
-    if (this.state.username && this.state.password) {
-      postData('login', this.state).then((result) => {
-        let responseJson = result;
-        if (responseJson.userData) {
-          sessionStorage.setItem('userData', JSON.stringify(responseJson));
-          this.setState({ redirectToReferrer: true });
-        } else {
-          alert(result.error);
-        }
-      });
-    }
+function loginCheck(username, password, setIsRedirected) {
+  if (username && password) {
+    postData('login', { username, password }).then((result) => {
+      let responseJson = result;
+      if (responseJson.userData) {
+        sessionStorage.setItem('userData', JSON.stringify(responseJson));
+        setIsRedirected(true);
+      } else {
+        alert(result.error);
+      }
+    });
   }
+}
 
-  handleInputChange(e) {
-    this.setState({ [e.target.name]: e.target.value });
-  }
+function handleInputChange(setState, e) {
+  setState(e.target.value);
 }
