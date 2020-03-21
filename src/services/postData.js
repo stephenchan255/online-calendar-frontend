35e9345ab2979288ad9388f2d2668139ae77e4b9
guoqing.chen <stephen.chan255@gmail.com>
2020-03-22 09:57:17 +1000

    modify form-control style

--- src/services/postData.js
+++ src/services/postData.js
@@ -1,5 +1,6 @@
 export function postData(type, data) {
-  let BaseURL = 'https://online-calendar-backend.herokuapp.com/';
+  // let BaseURL = 'https://online-calendar-backend.herokuapp.com/';
+  let BaseURL = 'http://localhost:8080/online-calendar-backend/';
   // let BaseURL = 'http://localhost:8080/react-calendar/public/index.php';
   return new Promise((resolve, reject) => {
     fetch(BaseURL + '?type=' + type, {
