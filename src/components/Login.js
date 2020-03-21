c5deada0ee8fc69de3fa2444623d4d8214e480bc
guoqing.chen <stephen.chan255@gmail.com>
2020-03-21 19:06:30 +1000

    intro useState to Signup.js

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,48 +1,27 @@
 import React, { useState } from 'react';
 import { Redirect } from 'react-router-dom';
-import { postData } from '../services/postData';
+import { auth, handleInputChange } from './Signup';
+// import { postData } from '../services/postData';
 
 export default function Login() {
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
-  const [isRedirected, setIsRedirected] = useState(false);
+  const [redirect, setRedirect] = useState(false); // control whether redirect to the homepage
 
-  if (isRedirected) {
+  if (redirect) {
     return (<Redirect to={'/home'} />)
   }
 
   return (
     <div className="content">
       <div className="form">
-        <div className="form-group">
-          <input type="text" name="username" placeholder="Username"
-            onChange={(e) => handleInputChange(setUsername, e)} className="form-control" />
-        </div>
-        <div className="form-group">
-          <input type="password" name="password" placeholder="Password"
-            onChange={(e) => handleInputChange(setPassword, e)} className="form-control" />
-        </div>
-        <input type="submit" value="Login" onClick={() => loginCheck(username, password, setIsRedirected)} className="btn" />
+        <input type="text" name="username" placeholder="Username" className="form-control"
+          onChange={(e) => handleInputChange(setUsername, e)} />
+        <input type="password" name="password" placeholder="Password" className="form-control"
+          onChange={(e) => handleInputChange(setPassword, e)} />
+        <input type="submit" value="Login" onClick={() => auth('emptyEmail', username, password, setRedirect, 'login')} className="btn" />
         <a href="/signup">Signup</a>
       </div>
-    </div>
+    </div >
   );
-}
-
-function loginCheck(username, password, setIsRedirected) {
-  if (username && password) {
-    postData('login', { username, password }).then((result) => {
-      let responseJson = result;
-      if (responseJson.userData) {
-        sessionStorage.setItem('userData', JSON.stringify(responseJson));
-        setIsRedirected(true);
-      } else {
-        alert(result.error);
-      }
-    });
-  }
-}
-
-function handleInputChange(setState, e) {
-  setState(e.target.value);
 }
--- src/components/Signup.js
+++ src/components/Signup.js
@@ -1,61 +1,48 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import { Redirect } from 'react-router-dom';
 import { postData } from '../services/postData';
 
-export default class Signup extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      username: '',
-      password: '',
-      email: '',
-      redirectToReferrer: false
-    };
-    this.handleInputChange = this.handleInputChange.bind(this);
-    this.signup = this.signup.bind(this);
+export default function Singup() {
+  const [email, setEmail] = useState('');
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [redirect, setRedirect] = useState(false); // control whether redirect to the homepage
+
+  if (redirect) {
+    return (<Redirect to={'/home'} />)
   }
 
-  render() {
-    if (this.state.redirectToReferrer || sessionStorage.getItem('userData')) {
-      return (<Redirect to={'/home'} />)
-    }
-
-    return (
-      <div className="content">
-        <div className="form">
-          <div className="form-group">
-            <input type="text" name="email" placeholder="Email" className="form-control" onChange={this.handleInputChange} />
-          </div>
-          <div className="form-group">
-            <input type="text" name="username" placeholder="Username" className="form-control" onChange={this.handleInputChange} />
-          </div>
-          <div className="form-group">
-            <input type="password" name="password" placeholder="Password" className="form-control" onChange={this.handleInputChange} />
-          </div>
-          <input type="submit" className="btn" value="Sign Up" onClick={this.signup} />
-          <a href="/">Login</a>
-        </div>
-
+  return (
+    <div className="content">
+      <div className="form">
+        <input type="text" name="email" placeholder="Email" className="form-control"
+          onChange={(e) => handleInputChange(setEmail, e)} />
+        <input type="text" name="username" placeholder="Username" className="form-control"
+          onChange={(e) => handleInputChange(setUsername, e)} />
+        <input type="password" name="password" placeholder="Password" className="form-control"
+          onChange={(e) => handleInputChange(setPassword, e)} />
+        <input type="submit" className="btn" value="Sign Up"
+          onClick={() => auth(email, username, password, setRedirect, 'signup')} />
+        <a href="/">Login</a>
       </div>
-    );
-  }
+    </div>
+  );
+}
 
-  signup() {
-    if (this.state.username && this.state.password
-      && this.state.email) {
-      postData('signup', this.state).then((result) => {
-        let responseJson = result;
-        if (responseJson.userData) {
-          sessionStorage.setItem('userData', JSON.stringify(responseJson));
-          this.setState({ redirectToReferrer: true });
-        } else {
-          alert(result.error);
-        }
-      });
-    }
+export function auth(email, username, password, setRedirect, type) {
+  if (email && username && password) {
+    postData(type, { email, username, password }).then((result) => {
+      let responseJson = result;
+      if (responseJson.userData) {
+        sessionStorage.setItem('userData', JSON.stringify(responseJson));
+        setRedirect(true);
+      } else {
+        alert(result.error);
+      }
+    });
   }
+}
 
-  handleInputChange(e) {
-    this.setState({ [e.target.name]: e.target.value });
-  }
+export function handleInputChange(setState, e) {
+  setState(e.target.value);
 }
