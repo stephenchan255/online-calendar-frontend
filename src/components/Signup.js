ee7e1885de46246c61e471c57d647fdb2a097249
guoqing.chen <stephen.chan255@gmail.com>
2020-03-22 15:54:42 +1000

    modify signup/login style

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -20,12 +20,12 @@ export default function Singup() {
         <div className="input-box">
           <input type="text" name="username" placeholder="Username" className="form-control"
             onChange={(e) => handleInputChange(setUsername, e)} />
-          <span>Username contains at least 4 characters, which can be letters, digits and underscore.</span>
+          <span>Username must contains at least 4 characters, which can be letters, digits and underscore.</span>
         </div>
         <div className="input-box">
           <input type="password" name="password" placeholder="Password" className="form-control"
             onChange={(e) => handleInputChange(setPassword, e)} />
-          <span>Password contains at least 8 characters, including a uppercase letter, a lowercase letter,
+          <span>Password must contains at least 8 characters, including a uppercase letter, a lowercase letter,
             a digit and a special character.</span>
         </div>
 
