48d393967f987e184a7b3e9e86ec59e181647842
guoqing.chen <stephen.chan255@gmail.com>
2020-03-22 12:08:19 +1000

    modify form style

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -17,10 +17,18 @@ export default function Singup() {
       <div className="form">
         <input type="text" name="email" placeholder="Email" className="form-control"
           onChange={(e) => handleInputChange(setEmail, e)} />
-        <input type="text" name="username" placeholder="Username" className="form-control"
-          onChange={(e) => handleInputChange(setUsername, e)} />
-        <input type="password" name="password" placeholder="Password" className="form-control"
-          onChange={(e) => handleInputChange(setPassword, e)} />
+        <div className="input-box">
+          <input type="text" name="username" placeholder="Username" className="form-control"
+            onChange={(e) => handleInputChange(setUsername, e)} />
+          <span>Username contains at least 4 characters, which can be letters, digits and underscore.</span>
+        </div>
+        <div className="input-box">
+          <input type="password" name="password" placeholder="Password" className="form-control"
+            onChange={(e) => handleInputChange(setPassword, e)} />
+          <span>Password contains at least 8 characters, including a uppercase letter, a lowercase letter,
+            a digit and a special character.</span>
+        </div>
+
         <input type="submit" className="btn" value="Sign Up"
           onClick={() => auth(email, username, password, setRedirect, 'signup')} />
         <div className="btn2" onClick={() => setRedirect('/')}>Login</div >
