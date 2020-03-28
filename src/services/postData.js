2f23d9cf8d390031c7788caeded807a0c62b1932
guoqing.chen <stephen.chan255@gmail.com>
2020-03-28 19:28:58 +1000

    simplify postData().return

--- src/services/postData.js
+++ src/services/postData.js
@@ -2,17 +2,13 @@ export function postData(type, data) {
   let BaseURL = 'https://online-calendar-backend.herokuapp.com/';
   // let BaseURL = 'http://localhost:8080/online-calendar-backend/';
   // let BaseURL = 'http://localhost:8080/react-calendar/public/index.php';
-  return new Promise((resolve, reject) => {
-    fetch(BaseURL + '?type=' + type, {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-        'Accept': 'application/json',
-      },
-      body: JSON.stringify(data)
-    })
-      .then(response => response.json())
-      .then(res => resolve(res))
-      .catch(error => reject(error));
-  });
+  return fetch(BaseURL + '?type=' + type, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      'Accept': 'application/json',
+    },
+    body: JSON.stringify(data)
+  })
+    .then(response => response.json());
 }
